ac82a4e8ab12a5664815a209e914161c470d5b9a
Liam Tolkkinen <83843957+liamtolkki@users.noreply.github.com>
2026-08-23 19:43:50 -0400

    Update companion recipe discovery expectations

--- src/test/java/dev/liamtolkkinen/sanctuary/sentry/SentryRecipeDiscoveryCatalogTest.java
+++ src/test/java/dev/liamtolkkinen/sanctuary/sentry/SentryRecipeDiscoveryCatalogTest.java
@@ -10,28 +10,34 @@ import org.junit.jupiter.api.Test;
 class SentryRecipeDiscoveryCatalogTest {
 
     @Test
-    void companionRecipesUseExpectedDiscoveryIngredients() {
+    void companionRecipesRetainRepresentativeUnlockIngredients() {
         assertMaterial(ExtendedItemIds.COMPANION_IRON_GOLEM, Material.CARVED_PUMPKIN);
         assertSpecial(
             ExtendedItemIds.COMPANION_PILLAGER,
-            SentryRecipeCatalog.SpecialIngredient.OMINOUS_BOTTLE_V
+            SentryRecipeCatalog.SpecialIngredient.OMINOUS_BANNER
         );
         assertMaterial(ExtendedItemIds.COMPANION_SKELETON, Material.SKELETON_SKULL);
         assertSpecial(
             ExtendedItemIds.COMPANION_PIGLIN_BRUTE,
             SentryRecipeCatalog.SpecialIngredient.PIGLIN_BRUTE_TROPHY_HEAD
         );
-        assertMaterial(ExtendedItemIds.COMPANION_EVOKER, Material.TOTEM_OF_UNDYING);
+        assertSpecial(
+            ExtendedItemIds.COMPANION_EVOKER,
+            SentryRecipeCatalog.SpecialIngredient.OMINOUS_BOTTLE_V
+        );
         assertSpecial(
             ExtendedItemIds.COMPANION_BABY_ZOMBIE,
             SentryRecipeCatalog.SpecialIngredient.ZOMBIE_TROPHY_HEAD
         );
-        assertMaterial(ExtendedItemIds.COMPANION_BLAZE, Material.GHAST_TEAR);
+        assertMaterial(ExtendedItemIds.COMPANION_BLAZE, Material.BLAZE_ROD);
         assertSpecial(
             ExtendedItemIds.COMPANION_CREEPER,
             SentryRecipeCatalog.SpecialIngredient.CREEPER_TROPHY_HEAD
         );
-        assertMaterial(ExtendedItemIds.COMPANION_WITHER, Material.NETHER_STAR);
+        assertMaterial(
+            ExtendedItemIds.COMPANION_WITHER,
+            Material.WITHER_SKELETON_SKULL
+        );
         assertMaterial(ExtendedItemIds.COMPANION_DROWNED, Material.TRIDENT);
         assertMaterial(ExtendedItemIds.COMPANION_GUARDIAN, Material.HEART_OF_THE_SEA);
         assertMaterial(ExtendedItemIds.COMPANION_ELDER_GUARDIAN, Material.CONDUIT);

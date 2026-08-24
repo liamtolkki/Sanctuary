e8c239e2fbf1043dc79b5e2e3cb78eefe8df6175
Liam Tolkkinen <83843957+liamtolkki@users.noreply.github.com>
2026-08-24 00:32:59 -0400

    Fix missing SanctuaryPosition import

--- src/main/java/dev/liamtolkkinen/sanctuary/anchor/AnchorBreakListener.java
+++ src/main/java/dev/liamtolkkinen/sanctuary/anchor/AnchorBreakListener.java
@@ -1,5 +1,6 @@
 package dev.liamtolkkinen.sanctuary.anchor;
 
+import dev.liamtolkkinen.sanctuary.sanctuary.SanctuaryPosition;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;

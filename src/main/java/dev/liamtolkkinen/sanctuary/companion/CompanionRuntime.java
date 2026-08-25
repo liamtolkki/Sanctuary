a5078bd07cabae60aa25e0b5c7f4ea82179d2a92
Liam Tolkkinen <83843957+liamtolkki@users.noreply.github.com>
2026-08-24 20:02:47 -0400

    Suppress managed elder guardian mining fatigue

--- src/main/java/dev/liamtolkkinen/sanctuary/companion/CompanionRuntime.java
+++ src/main/java/dev/liamtolkkinen/sanctuary/companion/CompanionRuntime.java
@@ -1,6 +1,7 @@
 package dev.liamtolkkinen.sanctuary.companion;
 
 import dev.liamtolkkinen.extendedui.ExtendedUI;
+import dev.liamtolkkinen.sanctuary.sentry.ManagedElderGuardianEffectListener;
 import java.util.Objects;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,6 +25,10 @@ public final class CompanionRuntime {
 
         plugin.getServer().getPluginManager().registerEvents(
             new CompanionListener(service, uiService, eggState, plugin, plugin.getLogger()),
+            plugin
+        );
+        plugin.getServer().getPluginManager().registerEvents(
+            new ManagedElderGuardianEffectListener(plugin),
             plugin
         );
         new CompanionTask(service, plugin.getLogger()).start(plugin);
